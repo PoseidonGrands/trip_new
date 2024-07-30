a119b3ef30d4f8110c0958c11787ffba16617d9e
PoseidonGrands <3253532864@qq.com~>
2024-07-30 20:20:40 +0800

    完成评论页面的下拉加载和上拉刷新的功能

--- src/views/sight/SightComment.vue
+++ src/views/sight/SightComment.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- 导航栏（提供返回 -->
   <van-nav-bar
     class="comment-list-nav"
     title="景点评论"
@@ -6,15 +7,34 @@
     left-arrow
     @click-left="goBack"
   />
-  <div class="comment-list-container">
+  <!-- 1、List控件实现数据下拉加载 -->
+  <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
+    <van-list
+      class="comment-list-container"
+      v-model:loading="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      v-model:error="error"
+      error-text="请求失败，点击重新加载"
+      offset="40"
+      @load="loadDataByVanList"
+    >
+      <!-- 评论列表 -->
+      <CommentItem v-for="item in sightComments" :key="item" :item="item" />
+    </van-list>
+  </van-pull-refresh>
+
+  <!-- 2、分页控件实现数据分页 -->
+  <!-- 评论列表 -->
+  <!-- <div class="comment-list-container">
     <CommentItem v-for="item in sightComments" :key="item" :item="item" />
-  </div>
-  <van-pagination
+  </div> -->
+  <!-- <van-pagination
     v-model="currentIndex"
     :total-items="totalItems"
     :items-per-page="itemsPerPage"
     @change="changePage"
-  />
+  /> -->
 </template>
 
 <script>
@@ -24,24 +44,62 @@ import { ajax } from '@/utils/ajax'
 export default {
   data() {
     return {
-      sightComments: {},
+      // 是否加载中
+      loading: false,
+      // 是否加载完成
+      finished: false,
+      // 是否显示错误提示
+      error: false,
+      // 是否在刷新中
+      refreshing: false,
+      sightComments: [],
       // 当前第几页
       currentIndex: 1,
       // 每页数据数
-      itemsPerPage: 6,
+      itemsPerPage: 5,
       // 总共数据量（用于计算有多少页
       totalItems: ''
     }
   },
   created() {
-    this.loadData()
+    // 通过路由获取景点id
+    this.id = this.$route.params.id
+    // this.loadDataByVanList()
   },
   components: {
     CommentItem
   },
   methods: {
-    loadData() {
-      this.id = this.$route.params.id
+    // 列表加载数据
+    loadDataByVanList() {
+      let url = SightApis.sightCommentUrl + '/' + this.id
+      ajax
+        .get(url, { params: { currentIndex: this.currentIndex, itemsPerPage: this.itemsPerPage } })
+        .then(({ data: { meta, objects } }) => {
+          console.log('开始加载')
+          // 数据拼接
+          this.sightComments = this.sightComments.concat(objects)
+          // 加载完成
+          this.loading = false
+          // 下一页的页码（用接口数据返回的当前页码更稳妥
+          this.currentIndex = meta.page_index + 1
+          // 判断数据是否加载完成
+          if (this.currentIndex > meta.total_pages) {
+            this.finished = true
+          }
+          this.refreshing = false
+          console.log('加载完成')
+        })
+        .catch((error) => {
+          console.log('res:', error)
+          // 停止加载
+          this.loading = false
+          this.error = true
+          this.refreshing = false
+        })
+    },
+    // 分页控件加载数据
+    loadDataByVanPagination() {
       let url = SightApis.sightCommentUrl + '/' + this.id
       ajax
         .get(url, { params: { currentIndex: this.currentIndex, itemsPerPage: this.itemsPerPage } })
@@ -51,9 +109,21 @@ export default {
           this.itemsPerPage = meta.per_page_count
         })
     },
+    // 页面刷新
+    onRefresh() {
+      // 重置全部状态变量
+      this.sightComments = []
+      this.currentIndex = 1
+      this.error = false
+      this.loading = false
+      this.finished = false
+
+      // 重新获取数据
+      this.loadDataByVanList()
+    },
     // 页面切换
     changePage() {
-      this.loadData()
+      this.loadDataByVanPagination()
     },
     goBack() {
       this.$router.go(-1)
